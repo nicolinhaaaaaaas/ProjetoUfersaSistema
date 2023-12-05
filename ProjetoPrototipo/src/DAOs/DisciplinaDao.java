768f8df4e62a81850cc4c14d33744d6dd813085c
nicolinhaaaaaaas <112559599+nicolinhaaaaaaas@users.noreply.github.com>
2023-12-05 03:39:18 -0300

    aff

--- ProjetoPrototipo/src/DAOs/DisciplinaDao.java
+++ ProjetoPrototipo/src/DAOs/DisciplinaDao.java
@@ -1,5 +1,25 @@
 package ProjetoPrototipo.src.DAOs;
 
+import java.sql.*;
+import java.util.*;
+
+import ProjetoPrototipo.src.ENTIDADES.*;
+
 public class DisciplinaDao {
     
+    private static Connection conexaoDisciplina;
+
+    public DisciplinaDao(Connection conexaoDisciplina){
+        DisciplinaDao.conexaoDisciplina = conexaoDisciplina;
+    }
+
+    public static void inserirDisciplina(Disciplina disciplina) throws SQLException{
+
+    }
+
+    public static Disciplina buscarDisciplinaPorId(int id) throws SQLException{
+
+    }
+
+    public
 }
